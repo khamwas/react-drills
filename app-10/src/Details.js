522115f63dd288ea4978adb94526890e98b891e5
Emmanuel Bravo <emmanuel@Emmanuels-MacBook-Pro.local>
2018-10-10 20:14:06 -0500

    added home button

--- app-10/src/Details.js
+++ app-10/src/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./Product.css";
+import { Link } from "react-router-dom";
 
 class Details extends Component {
   constructor() {
@@ -24,18 +25,26 @@ class Details extends Component {
   render() {
     let display = this.state.product.map(elem => {
       return (
-        <div className="outerCont">
+        <div key={elem.id} className="outerCont">
           {" "}
           <div className="productCont">
             <h2>{elem.title}</h2>
             <h4>{elem.desc}</h4>
             <img className="productImg" src={elem.image} alt="elem.title" />
-            <h4>{elem.price}</h4>
+            <h4>Price: ${elem.price}</h4>
           </div>
         </div>
       );
     });
-    return <div>{display}</div>;
+    return (
+      <div>
+        <div>{display}</div>
+        <br />
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+      </div>
+    );
   }
 }
 
